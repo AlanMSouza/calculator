65239e4d3313c2055432103ba8cfb1e7bcbfb6e1
Alan M. Souza <99767498+AlanMSouza@users.noreply.github.com>
2023-08-03 17:02:41 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const botao = document.querySelectorAll("#botao");
+
+botao.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        
+    })
+})
+
+function botao() {
+    
+}
